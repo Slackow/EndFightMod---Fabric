2d7a5cf88572311611d1d6286d2c5351092d010f
pixfumy <asaddurrani1@live.ca>
2022-03-10 07:45:50 -0500

    temporary workaround for 1/4 vs 1/2

--- src/main/java/com/slackow/endfight/mixin/EnderDragonEntityMixin.java
+++ src/main/java/com/slackow/endfight/mixin/EnderDragonEntityMixin.java
@@ -2,14 +2,20 @@ package com.slackow.endfight.mixin;
 
 import com.slackow.endfight.EndFightMod;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.boss.dragon.EnderDragonEntity;
+import net.minecraft.entity.boss.dragon.EnderDragonPart;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.vehicle.MinecartEntity;
 import net.minecraft.text.LiteralText;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.time.LocalTime;
@@ -19,6 +25,12 @@ import static net.minecraft.util.math.MathHelper.clamp;
 
 @Mixin(EnderDragonEntity.class)
 public abstract class EnderDragonEntityMixin extends LivingEntity {
+    @Shadow private Entity target;
+    @Shadow public double field_3742;
+    @Shadow public double field_3751;
+    @Shadow public double field_3752;
+    private int ticksSincePickedTarget = 0;
+
     public EnderDragonEntityMixin(World world) {
         super(world);
     }
@@ -35,5 +47,18 @@ public abstract class EnderDragonEntityMixin extends LivingEntity {
                 EndFightMod.time = 0;
             }
         }
+    }
+
+    @Inject(method = "method_2906", at = @At("HEAD"))
+    public void setPickedTargetLastTick(CallbackInfo ci) {
+        if (this.ticksSincePickedTarget == 1) {
+            MinecraftClient.getInstance().player.sendMessage(new LiteralText("1/4"));
+        }
+        this.ticksSincePickedTarget = 0;
+    }
+
+    @Inject(method = "tickMovement", at = @At("TAIL"))
+    public void incrementTicks(CallbackInfo ci) {
+        this.ticksSincePickedTarget += 1;
     }
 }
