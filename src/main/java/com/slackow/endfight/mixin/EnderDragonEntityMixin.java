679a698540704c26acff566282e4357380746ad5
pixfumy <asaddurrani1@live.ca>
2022-03-10 12:00:23 -0500

    add sharpie's code with suggested changes

--- src/main/java/com/slackow/endfight/mixin/EnderDragonEntityMixin.java
+++ src/main/java/com/slackow/endfight/mixin/EnderDragonEntityMixin.java
@@ -1,5 +1,6 @@
 package com.slackow.endfight.mixin;
 
+import com.google.common.collect.Lists;
 import com.slackow.endfight.EndFightMod;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
@@ -7,6 +8,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.boss.dragon.EnderDragonEntity;
 import net.minecraft.entity.boss.dragon.EnderDragonPart;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.vehicle.MinecartEntity;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.math.MathHelper;
@@ -14,12 +16,16 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Iterator;
+import java.util.List;
 
 import static net.minecraft.util.math.MathHelper.clamp;
 
@@ -48,17 +54,46 @@ public abstract class EnderDragonEntityMixin extends LivingEntity {
             }
         }
     }
-
-    @Inject(method = "method_2906", at = @At("HEAD"))
-    public void setPickedTargetLastTick(CallbackInfo ci) {
-        if (this.ticksSincePickedTarget == 1 && this.target != null) {
-            MinecraftClient.getInstance().player.sendMessage(new LiteralText("1/4"));
-        }
-        this.ticksSincePickedTarget = 0;
+    
+    int setNewTargetCounter = 0; // increment this every time you call setNewTarget
+    int lastSetNewTargetCount = 0;
+    private double distanceTo(double targetX, double targetY, double targetZ) {
+        double o;
+        double p;
+        double q;
+        double r;
+        o = targetX - this.x;
+        p = targetY - this.y;
+        q = targetZ - this.z;
+        r = o * o + p * p + q * q;
+        return Math.sqrt(r);
     }
 
-    @Inject(method = "tickMovement", at = @At("TAIL"))
-    public void incrementTicks(CallbackInfo ci) {
-        this.ticksSincePickedTarget += 1;
+    @Inject(method = "tickMovement", at = @At("HEAD"))
+    public void onUpdates(CallbackInfo ci) {
+        if (lastSetNewTargetCount != setNewTargetCounter) {
+            lastSetNewTargetCount = setNewTargetCounter;
+            List<PlayerEntity> list = Lists.newArrayList((Iterable)this.world.playerEntities);
+            PlayerEntity player = list.get(0);
+            double targetX = player.x;
+            double targetZ = player.z;
+            double s = targetX - this.x;
+            double t = targetZ - this.z;
+            double u = Math.sqrt(s * s + t * t);
+            double v = 0.4000000059604645D + u / 80.0D - 1.0D;
+            if (v > 10.0D) {
+                v = 10.0D;
+            }
+            double targetY = ((Entity)(player)).getBoundingBox().minY + v;
+            if (this.distanceTo(targetX, targetY, targetZ) >= 10.0 && this.distanceTo(targetX, targetY, targetZ) <= 150.0D) {
+                System.out.println("you got the strat");
+                player.sendMessage(new LiteralText("You got the strat"));
+            }
+        }
+    }
+
+    @Inject(method = "method_2906", at = @At("TAIL"))
+    public void newTarget(CallbackInfo ci){
+        setNewTargetCounter++;
     }
 }
