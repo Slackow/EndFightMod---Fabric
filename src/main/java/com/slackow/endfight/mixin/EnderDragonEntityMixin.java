778ec07bdb0571d219153b25a0735cdd9ee7e446
pixfumy <asaddurrani1@live.ca>
2022-03-10 08:02:14 -0500

    port to 1.7

--- src/main/java/com/slackow/endfight/mixin/EnderDragonEntityMixin.java
+++ src/main/java/com/slackow/endfight/mixin/EnderDragonEntityMixin.java
@@ -24,78 +24,39 @@ import static net.minecraft.util.math.MathHelper.clamp;
 @Mixin(EnderDragonEntity.class)
 public abstract class EnderDragonEntityMixin extends LivingEntity {
     @Shadow private Entity target;
-
     @Shadow public double field_3742;
-
     @Shadow public double field_3751;
-
     @Shadow public double field_3752;
+    private int ticksSincePickedTarget = 0;
 
     public EnderDragonEntityMixin(World world) {
         super(world);
     }
 
     @Inject(method = "method_6302", at = @At("RETURN"))
-    public void onDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+    public void onDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir){
         if (cir.getReturnValue()) {
-            if (getSelectedConfig().damageInfo) {
-                MinecraftClient.getInstance().field_3805.sendMessage(new LiteralText("Dragon damaged by " + source.getName() + ": " + amount));
-            }
+            MinecraftClient.getInstance().field_3805.sendMessage(new LiteralText("Dragon damaged by " + source.getName() + ": " + amount));
             if (getHealth() <= 0) {
                 int seconds = (int) ((System.currentTimeMillis() - EndFightMod.time) / 1000);
                 seconds = clamp(seconds, 0, 86399);
                 MinecraftClient.getInstance().field_3805.sendMessage(
                         new LiteralText("Dragon Killed in about " + LocalTime.ofSecondOfDay(seconds).format(DateTimeFormatter.ofPattern("mm:ss")) + " [RTA]"));
-                EndFightMod.time = System.currentTimeMillis();
+                EndFightMod.time = 0;
             }
         }
     }
-    boolean isFarFromPlayer = false;
 
-    private static double r(double a) {
-        return Math.round(a * 100) / 100.0;
-    }
-
-    boolean isPast150 = false;
-    boolean isFirstPlayerTarget = true;
-    int setNewTargetCounter; // increment this every time you call setNewTarget
-    int lastSetNewTargetCount;
-
-    @Inject(method = "tickMovement", at = @At("HEAD"))
-    public void onUpdates(CallbackInfo ci) {
-
-        double targetX = field_3742, targetY = field_3751, targetZ = field_3752;
-
-        if (lastSetNewTargetCount != setNewTargetCounter) {
-
-            lastSetNewTargetCount = setNewTargetCounter;
-
-            if (!isPast150 && isFirstPlayerTarget) {
-
-                if (this.distanceTo(targetX, targetY, targetZ) >= 10.0) {
-                    System.out.println("you got the strat");
-                    MinecraftClient.getInstance().field_3805.sendMessage(new LiteralText("You got the strat"));
-                }
-            }
-
-            isFirstPlayerTarget = false;
-        } else if (this.distanceTo(targetX, targetY, targetZ) >= 150.0) {
-            isPast150 = true;
-            isFirstPlayerTarget = true;
-        } else {
-            isPast150 = false;
-            isFirstPlayerTarget = true;
+    @Inject(method = "method_2906", at = @At("HEAD"))
+    public void setPickedTargetLastTick(CallbackInfo ci) {
+        if (this.ticksSincePickedTarget == 1) {
+            MinecraftClient.getInstance().field_3805.sendMessage(new LiteralText("1/4"));
         }
+        this.ticksSincePickedTarget = 0;
     }
 
-    @Inject(method = "method_2906", at = @At("TAIL"))
-    public void newTarget(CallbackInfo ci){
-        setNewTargetCounter++;
-        System.out.println("------------------");
-        System.out.println(setNewTargetCounter);
-        System.out.println("dragon pos " + r(x) + " " + r(y) + " " + r(z) + " ");
-        System.out.println("target obj " + target);
-        System.out.println("target coords " + r(field_3742) + " " + r(field_3751) + " " + r(field_3752));
+    @Inject(method = "tickMovement", at = @At("TAIL"))
+    public void incrementTicks(CallbackInfo ci) {
+        this.ticksSincePickedTarget += 1;
     }
-
 }
